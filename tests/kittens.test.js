09542e58ae435d5d6cef48eaba9e5388ad68d9cb
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-10-10 11:53:30 +0800

    Add new tests for routes

--- tests/kittens.test.js
+++ tests/kittens.test.js
@@ -4,18 +4,17 @@ const mongoose = require("mongoose");
 const Kitten = require("../models/Kitten");
 const {MongoMemoryServer} = require("mongodb-memory-server");
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
 describe("kittens", () => {
   let mongoServer;
   beforeAll(async () => {
     try {
       mongoServer = new MongoMemoryServer();
       const mongoUri = await mongoServer.getConnectionString();
-
-      mongoose.set("useNewUrlParser", true);
-      mongoose.set("useFindAndModify", false);
-      mongoose.set("useCreateIndex", true);
-      mongoose.set("useUnifiedTopology", true);
-
       await mongoose.connect(mongoUri);
     } catch (err) {
       console.error(err);
@@ -36,6 +35,10 @@ describe("kittens", () => {
     await Kitten.create(kittens);
   });
 
+  afterEach(async () => {
+    await Kitten.remove();
+  });
+
   describe("[GET] /kittens", () => {
     it("gets all kittens", async () => {
       const expectedKittens = [
@@ -52,5 +55,37 @@ describe("kittens", () => {
         expect(actualKittens[index]).toEqual(expect.objectContaining(kitten));
       });
     });
+  });
+
+  describe("[POST] /kittens/new", () => {
+    it("adds a new kitten", async () => {
+      const expectedKittens = [
+        {name: "fluffy", age: 5, sex: "male"},
+        {name: "muffin", age: 3, sex: "female"},
+        {name: "waffle", age: 7, sex: "female"},
+        {name: "minty", age: 3, sex: "female"}
+      ];
+
+      await request(app)
+        .post("/kittens/new")
+        .send({name: "Minty", age: 3, sex: "female"})
+        .expect(200);
+
+      const {body: kittens} = await request(app)
+        .get("/kittens")
+        .expect(200);
+
+      kittens.forEach((kitten, index) => {
+        expect(kitten).toEqual(expect.objectContaining(expectedKittens[index]));
+      });
+    });
+  });
+
+  describe("[PUT] /kittens/:name", () => {
+    it("updates a kitten", () => {});
+  });
+
+  describe("[DELETE] /kittens/:name", () => {
+    it("removes a kitten", () => {});
   });
 });
