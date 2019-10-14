e758078319927be9c994c893a60c65842cd0cd45
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-10-14 15:36:39 +0800

    Add dependencies

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 if (app.get("env") !== "test") {
   require("./db");
 }
+
+const corsOptions = {
+  credentials: true,
+  allowedHeaders: "content-type",
+  origin: "http://localhost:3001"
+};
+
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
 
 app.use(express.json());
 
